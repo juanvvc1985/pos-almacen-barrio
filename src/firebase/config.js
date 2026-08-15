d182f4014a30eb84f39a8c56b92031cdef39be69
juanvvc1985 <juanvvc1985@gmail.com>
2026-08-15 13:25:35 -0400

    qwen

--- src/firebase/config.js
+++
@@ -1,9 +0,0 @@
-// Configuración de Firebase - usa firebase.js para inicialización
-export const firebaseConfig = {
-  apiKey: "AIzaSyAQUD8KyWSPYNz73RTrdSy-jZ3Lf2QiF3c",
-  authDomain: "almacen-de-barrio-a947a.firebaseapp.com",
-  projectId: "almacen-de-barrio-a947a",
-  storageBucket: "almacen-de-barrio-a947a.firebasestorage.app",
-  messagingSenderId: "1014856587704",
-  appId: "1:1014856587704:web:a4dbcdfaea21e88388974d"
-};
